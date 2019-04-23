2abeb8194746e467ea0e33b8477e718150a4de98
Paul Kim <paul@terra.money>
2019-04-23 19:59:08 +0900

    Added Columbus-1

--- src/networks.js
+++ src/networks.js
@@ -1,5 +1,12 @@
 const networks = [
   {
+    value: 'columbus-1',
+    key: 'Columbus-1',
+    lcd: 'https://lcd.terra.dev',
+    rpc: 'https://rpc.terra.dev',
+    wss: 'ws://rpc.terra.dev/websocket'
+  },
+  {
     value: 'soju-0008',
     key: 'Soju-0008',
     lcd: 'https://soju-0008-lcd.terra.dev',
@@ -7,18 +14,18 @@ const networks = [
     wss: 'ws://soju-0008-rpc.terra.dev/websocket'
   },
   {
-    value: `vodka`,
-    key: `Vodka Testnet`,
-    lcd: `https://vodka-lcd.terra.money`,
-    rpc: `https://vodka-rpc.terra.money`,
-    wss: `wss://vodka.terra.money:26657/websocket`
+    value: 'vodka',
+    key: 'Vodka Testnet',
+    lcd: 'https://vodka-lcd.terra.money',
+    rpc: 'https://vodka-rpc.terra.money',
+    wss: 'wss://vodka.terra.money:26657/websocket'
   },
   {
-    value: `columbus-drill-2`,
-    key: `Columbus Drill 2`,
-    lcd: `https://drill-2-lcd.terra.money`,
-    rpc: `https://drill-2-rpc.terra.money`,
-    wss: `wss://drill.terra.money:26657/websocket`
+    value: 'columbus-drill-2',
+    key: 'Columbus Drill 2',
+    lcd: 'https://drill-2-lcd.terra.money',
+    rpc: 'https://drill-2-rpc.terra.money',
+    wss: 'wss://drill.terra.money:26657/websocket'
   }
 ];
 
